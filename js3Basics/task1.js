8960a385970c89d064b03a59047839ac6bdcb86d
Daria Habriielian <dariahabriielian@MacBook-Air-Daria.local>
2026-02-19 14:24:42 +0100

    Add js3basics

---
+++ js3Basics/task1.js
@@ -0,0 +1,11 @@
+const courseName = "AQA-JS";    // string
+const lessonNumber = 4;         // number
+const isAdvanced = true;        // boolean
+const emptyValue = null;        // null
+let notDefined;                 // undefined
+
+console.log(courseName);
+console.log(lessonNumber);
+console.log(isAdvanced);
+console.log(emptyValue);
+console.log(notDefined);
---
+++ js3Basics/task4.js
@@ -0,0 +1,16 @@
+// 4.1 Площа кола
+const circleRadius = 8;
+const circleArea = Math.PI * circleRadius ** 2;
+console.log("Circle area:", circleArea.toFixed(2));
+
+// 4.2 Площа прямокутника
+const lengthRectangle = 9;
+const widthRectangle = 4;
+const rectangleArea = lengthRectangle * widthRectangle;
+console.log("Rectangle area:", rectangleArea.toFixed(2));
+
+// 4.3 Обʼєм циліндра
+const cylinderRadius = 3.5;
+const heightCylinder = 8;
+const cylinderVolume = Math.PI * cylinderRadius ** 2 * heightCylinder;
+console.log("Cylinder volume:", cylinderVolume.toFixed(2));
