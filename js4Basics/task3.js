4152016acced6de7f802859f40b7018121984603
Daria Habriielian <dariahabriielian@MacBook-Air-Daria.local>
2026-02-19 14:29:26 +0100

    Add js4basics

---
+++ js4Basics/task3.js
@@ -0,0 +1,17 @@
+const number = 6;
+
+console.log("Multiplication table using for:");
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${number} x ${i} = ${number * i}`);
+}
+
+console.log("Multiplication table using while:");
+
+let i = 1;
+
+while (i <= 10) {
+  console.log(`${number} x ${i} = ${number * i}`);
+  i++;
+}
+
